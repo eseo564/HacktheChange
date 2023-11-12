ea80b0648c217647f3a6a8ab8c3a897316e19992
TRussell602 <113271487+TRussell602@users.noreply.github.com>
2023-11-12 11:21:46 -0700

    Moved edit employee script to separate file

---
+++ logic/edit_employees.js
@@ -0,0 +1,39 @@
+        let Wrkdays = [];
+        let i = 0;
+
+      document.getElementById("inputDay").addEventListener("click", processUserInput);
+
+        function processUserInput(manDayOfWk) {
+                var manDayOfWk = "";
+                while (manDayOfWk != 'done' && i < 5) {
+                    manDayOfWk = prompt("Enter a mandatory work day");
+		    manDayOfWk = manDayOfWk.toLowerCase();
+                    if (manDayOfWk == 'done') {
+                        printEnteredDays();
+                    } else {
+                        switch (manDayOfWk) {
+                            case 'monday':
+                            case 'tuesday':
+                            case 'wednesday':
+                            case 'thursday':
+                            case 'friday':
+                                Wrkdays[i] = manDayOfWk;
+                                i++;
+                                break;
+                            default:
+                                console.log("Invalid input. Please enter a valid day of the week.");
+                        }
+
+                    }
+                }
+            }
+
+        function printEnteredDays() {
+            const enteredDaysDiv = document.getElementById('enteredDays');
+            enteredDaysDiv.innerHTML = "<p>This employee is scheduled in office for these days:</p>";
+            Wrkdays.forEach(day => {
+                if (day !== null && day !== undefined) {
+                    enteredDaysDiv.innerHTML += `<p>${day}</p>`;
+                }
+            });
+        }
